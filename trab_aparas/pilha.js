d0cc923c2ec270659360293e178c85b0087693bf
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-05-12 10:26:12 -0300

    Update pilha.js

--- trab_aparas/pilha.js
+++ trab_aparas/pilha.js
@@ -6,7 +6,7 @@ class pilha {
   }
 
   push(cnpj, razao_social, nome_fantasia, email, telefone) {
-    let no_trab = new node_fornecedor(cnpj, razao_social, nome_fantasia, email, telefone);
+    let no_trab = new Node_fornecedor(cnpj, razao_social, nome_fantasia, email, telefone);
     if (this.size == 0) {
       this.head = no_trab;
       this.tail = no_trab;
@@ -38,10 +38,16 @@ class pilha {
 
   imprimePilha() {
     let no_trab = this.head;
+    let retorno = "";
     if (this.size == 0) {
       console.log("Lista vazia");
     } else {
       for(let i = 0; i < this.size; i++) {
-        console.log("CNPJ: " + no_trab.cnpj + "\nRazão Social: " + no_trab.razao_social + "\nNome Fantasia: " + no_trab.nome_fantasia + "\nEmail: " + no_trab.email + "\nTelefone: " + no_trab.telefone + "\n");
+        retorno += ("CNPJ: " + no_trab.cnpj + "\nRazão Social: " + no_trab.razao_social + "\nNome Fantasia: " + no_trab.nome_fantasia + "\nEmail: " + no_trab.email + "\nTelefone: " + no_trab.telefone + "\n\n");
         no_trab = no_trab.next;
       }
+      console.log(retorno);
+      return retorno;
+    }
+  }
+}
